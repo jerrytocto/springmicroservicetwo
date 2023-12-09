9e6f3cb0ca5cc6b82a07ac15390c8638dad8381e
JerryDev <132033940+jerrytocto@users.noreply.github.com.>
2023-12-08 23:14:51 -0500

    config ofis of eurekaserver

--- bikemcsv/src/main/java/com/jerrydev/bikemcsv/BikemcsvApplication.java
+++ bikemcsv/src/main/java/com/jerrydev/bikemcsv/BikemcsvApplication.java
@@ -2,8 +2,10 @@ package com.jerrydev.bikemcsv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BikemcsvApplication {
 
 	public static void main(String[] args) {
---
+++ eurekaconfig/src/test/java/com/jerrydev/eurekaconfig/EurekaconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerrydev.eurekaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
